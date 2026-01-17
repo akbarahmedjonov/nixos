3bc7955e6e569ffda88be66b5d174c7d7fb1b483
akbarahmedjonov <teamender1991@gmail.com>
2026-01-17 09:15:15 +0500

    first commit jala

---
+++ configuration.nix
@@ -0,0 +1,80 @@
+{ config, lib, pkgs, ... }:
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  networking.hostName = "linux";
+  networking.networkmanager.enable = true;
+  time.timeZone = "Asia/Tashkent";
+  services.getty.autologinUser = "user";
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+  users.users.akbar = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "audio" "video" "input" ];
+    shell = pkgs.fish;
+    packages = with pkgs; [
+      tree
+    ];
+  };
+  programs.fish.enable = true;
+  services.gvfs.enable = true;
+  services.power-profiles-daemon.enable = true;
+  services.tumbler.enable = true;
+  hardware.bluetooth.enable = true;
+  networing.firewall.enable = false;
+  services.upower.enable = false;
+  services.logind.settings.Login = {
+    HandleSuspendKey = "ignore";
+    HandleSuspendKeyLongPress= "ignore";
+  };
+  specialisation = {
+  nvidia.configuration = {
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.graphics.enable = true;
+    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+    hardware.nvidia.open = false;
+    hardware.nvidia.prime = {
+        nvidiaBusId = "PCI:2:0:0";
+        intelBusId = "PCI:0:2:0";
+      };
+    };
+  };
+  environment.systemPackages = with pkgs; [
+    starship
+    zoxide
+    vim
+    adw-bluetooth
+    brave
+    wget
+    curl
+    kitty
+    waybar
+    git
+    swww
+  ];
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+  ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true; 
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+  system.stateVersion = "25.11";
+  nixpkgs.config.allowUnfree = true;
+}
+
---
+++ home.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+let
+  dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  configs = {
+    hypr = "hypr";
+    nvim = "nvim";
+    wofi = "wofi";
+    rofi = "rofi";
+    foot = "foot";
+    waybar = "waybar";
+  };
+in
+{
+  imports = [
+    ./modules/theme.nix
+  ];
+
+  home.username = "akbar";
+  home.homeDirectory = "/home/akbar";
+  home.stateVersion = "25.11";
+  home.packages = with pkgs; [
+    hypridle
+    hyprlock
+    hyprsunset
+    hyprshot
+    hyprpolkitagent
+    neovim
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    gcc
+    nitch
+    rofi
+    pcmanfm
+    (pkgs.writeShellApplication {
+      name = "ns";
+      runtimeInputs = with pkgs; [
+        fzf
+        (nix-search-tv.overrideAttrs {
+          env.GOEXPERIMENT = "jsonv2";
+        })
+      ];
+      text = ''exec "${pkgs.nix-search-tv.src}/nixpkgs.sh" "$@"'';
+    })
+  ];
+  xdg.configFile = builtins.mapAttrs
+    (name: subpath: {
+      source = create_symlink "${dotfiles}/${subpath}";
+      recursive = true;
+    })
+    configs;
+}
